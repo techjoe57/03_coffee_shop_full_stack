e4e82665bbe630dc89eafcd0cdf77133bdb54f7f
T3chJo357 <47719750+techjoe57@users.noreply.github.com>
2022-10-11 15:32:52 +0000

    modify models.py

--- backend/settings.py
+++ backend/settings.py
@@ -1,5 +1,8 @@
 from os import environ
+from dotenv import load_dotenv
+#use pip install python-dotenv instead of pip install dotenv
 
+load_dotenv()
 #AUTH0 
 AUTH0_DOMAIN = environ.get("AUTH0_DOMAIN")
 ALGORITHMS = environ.get("ALGORITHMS")
